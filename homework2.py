069d412076107d000df7b53b7014b6807d639f52
Hugo96870 <hugo@ASUS.localdomain>
2021-10-23 15:31:05 +0100

    s

--- homework2.py
+++ homework2.py
@@ -2,7 +2,14 @@
 # Bernardo Castico ist196845
 # Hugo Rita ist196870
 
-import numpy as np
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import mutual_info_classif
+
+#Res = the 10-fold cross validation with our group number (117)
+Res = KFold(n_splits=10, random_state=117, shuffle=True)
 
 def getDataToMatrix(lines):
     realLines = []
@@ -26,14 +33,52 @@ def getDataToMatrix(lines):
             data += [realLines[i]]
     return data
 
+def splitData(list):
+    a = []
+    b = []
+    for i in list:
+        a.append(i[:-1])
+        b.append(i[-1])
+    return [a,b]
+
+
 def main():
     res = []
-
-    with open("HW1.txt") as f:
+    with open("HW2.txt") as f:
         lines = f.readlines()
     for line in lines:
         tmp = line.split(',')
         res.append(tmp)
     data = getDataToMatrix(res)
+
+    for i in [1,3,5,9]:
+        testData = []
+        trainData = []
+        accuracies1 = []
+        accuracies2 = []
+
+        for train, test in Res.split(data):
+            for j in test:
+                testData += [data[j]]
+            for j in train:
+                trainData += [data[j]]
+            trainDataSplit = splitData(trainData)
+            testDataSplit = splitData(testData)
+
+            decision = SelectKBest(mutual_info_classif, k=i).fit(trainDataSplit[0], trainDataSplit[1])
+            decisionTrainData = decision.transform(trainDataSplit[0])
+            decisionTestData = decision.transform(testDataSplit[0])
+
+            result1 = DecisonTreeClassifier(max_depth=None, criterion="entropy")
+            result2 = DecisonTreeClassifier(max_depth=i, criterion="entropy")
+
+            result1.fit(decisionTrainData, trainDataSplit[1])
+            result2.fit(decisionTrainData, trainDataSplit[1])
+
+            result1.Predict(testData)
+            result2.Predict(testData)
+
+            result1.Predict(trainData)
+            result2.Predict(trainData)
 
 main()
