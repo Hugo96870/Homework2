c11713727328a54bf4ea9f58eb8dfe7d35460eb5
Hugo96870 <hugo@ASUS.localdomain>
2021-10-23 17:37:32 +0100

    fim gráfico

--- homework2.py
+++ homework2.py
@@ -3,10 +3,10 @@
 # Hugo Rita ist196870
 
 from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import KFold
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import mutual_info_classif
+import matplotlib.pyplot as plt
 
 #Res = the 10-fold cross validation with our group number (117)
 Res = KFold(n_splits=10, random_state=117, shuffle=True)
@@ -43,7 +43,19 @@ def splitData(list):
 
 
 def main():
+    depthTestX = []
+    depthTestY = []
+    depthTrainX = []
+    depthTrainY = []
+    AllFeaturesTestX = []
+    AllFeaturesTestY = []
+    AllFeaturesTrainX = []
+    AllFeaturesTrainY = []
+
     res = []
+    finalAccuraciesDepth = []
+    finalAccuraciesAllFeatures = []
+
     with open("HW2.txt") as f:
         lines = f.readlines()
     for line in lines:
@@ -52,12 +64,22 @@ def main():
     data = getDataToMatrix(res)
 
     for i in [1,3,5,9]:
+        counter11 = 0
+        counter12 = 0
+        counter22 = 0
+        counter21 = 0
+
         testData = []
         trainData = []
-        accuracies1 = []
-        accuracies2 = []
+        accuraciesDepth = []
+        accuraciesAllFeatures = []
 
         for train, test in Res.split(data):
+            accuracyAuxDepthTest = 0
+            accuracyAuxDepthTrain = 0
+            accuracyAuxAllFeaturesTest = 0
+            accuracyAuxAllFeaturesTrain = 0
+
             for j in test:
                 testData += [data[j]]
             for j in train:
@@ -69,16 +91,67 @@ def main():
             decisionTrainData = decision.transform(trainDataSplit[0])
             decisionTestData = decision.transform(testDataSplit[0])
 
-            result1 = DecisonTreeClassifier(max_depth=None, criterion="entropy")
-            result2 = DecisonTreeClassifier(max_depth=i, criterion="entropy")
+            resultDepth = tree.DecisionTreeClassifier(max_depth=i, criterion="gini", max_features=None)
+            resultAllFeatures = tree.DecisionTreeClassifier(max_depth=None, criterion="gini", max_features=None)
 
-            result1.fit(decisionTrainData, trainDataSplit[1])
-            result2.fit(decisionTrainData, trainDataSplit[1])
 
-            result1.Predict(testData)
-            result2.Predict(testData)
+            resultDepth.fit(trainDataSplit[0], trainDataSplit[1])
+            resultAllFeatures.fit(decisionTrainData, trainDataSplit[1])
 
-            result1.Predict(trainData)
-            result2.Predict(trainData)
+            predictionsTest = resultDepth.predict(testDataSplit[0])
+            predictionsTrain = resultDepth.predict(trainDataSplit[0])
+
+            for j in range(len(predictionsTest)):
+                if predictionsTest[j] == testDataSplit[1][j]:
+                    accuracyAuxDepthTest += 1
+            for j in range(len(predictionsTrain)):
+                if predictionsTrain[j] == trainDataSplit[1][j]:
+                    accuracyAuxDepthTrain += 1
+            accuraciesDepth += [[accuracyAuxDepthTest/len(predictionsTest), accuracyAuxDepthTrain/len(predictionsTrain)]]
+
+            predictionsTest = resultAllFeatures.predict(decisionTestData)
+            predictionsTrain = resultAllFeatures.predict(decisionTrainData)
+
+            for j in range(len(predictionsTest)):
+                if predictionsTest[j] == testDataSplit[1][j]:
+                    accuracyAuxAllFeaturesTest += 1
+            for j in range(len(predictionsTrain)):
+                if predictionsTrain[j] == trainDataSplit[1][j]:
+                    accuracyAuxAllFeaturesTrain += 1
+            accuraciesAllFeatures += [[accuracyAuxAllFeaturesTest/len(predictionsTest), accuracyAuxAllFeaturesTrain/len(predictionsTrain)]]
+
+        for k in range(len(accuraciesDepth)):
+            counter11 += accuraciesDepth[k][0]
+            counter12 += accuraciesDepth[k][1]
+            counter21 += accuraciesAllFeatures[k][0]
+            counter22 += accuraciesAllFeatures[k][1]
+
+        finalAccuraciesDepth += [[counter11 / 10, counter12 / 10]]
+        finalAccuraciesAllFeatures += [[counter21 / 10, counter22 / 10]]
+
+    print(finalAccuraciesDepth)
+    print(finalAccuraciesAllFeatures)
+
+    #Plot
+    for i in range(4):
+        depthTestX = [1,3,5,9]
+        depthTestY += [finalAccuraciesDepth[i][0]]
+        depthTrainX = [1, 3, 5, 9]
+        depthTrainY += [finalAccuraciesDepth[i][1]]
+        AllFeaturesTestX = [1,3,5,9]
+        AllFeaturesTestY += [finalAccuraciesAllFeatures[i][0]]
+        AllFeaturesTrainX = [1, 3, 5, 9]
+        AllFeaturesTrainY += [finalAccuraciesAllFeatures[i][1]]
+
+    plt.xlabel('Depth/AllFeatures')
+    plt.ylabel('Accuracies')
+    plt.title('AP HW2')
+    plt.plot(depthTestX, depthTestY, label = "depth test")
+    plt.plot(depthTrainX, depthTrainY, label = "depth train")
+    plt.plot(AllFeaturesTestX, AllFeaturesTestY, label = "All Features test")
+    plt.plot(AllFeaturesTrainX, AllFeaturesTrainY, label = "All Features train")
+    plt.legend()
+
+    plt.show()
 
 main()
